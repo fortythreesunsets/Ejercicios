97d7733f4bf1bb2fe1218e0d01d258e2db3898c8
fortythreesunsets <sruv_2601@hotmail.com>
2022-06-18 16:44:10 -0500

    Update ejercicio 4

--- Intensivo/ejercicio4.py
+++ Intensivo/ejercicio4.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-api_key = '34c578a5b07e88ca96f7657935d1d610'
+api_key = '' # agregar key de openweathermap
 ciudad = input('Ingresa la ciudad de la que quieres saber la temperatura: ')
 url = f'https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric'
 
